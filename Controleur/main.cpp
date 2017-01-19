83d906f1ff8cce743fd1acb7bb0f76fd872d6246
Le Fur Pierre-Henri <lpierre-henri@linux.home>
2017-01-19 03:37:07 +0100

    Minor fixes

--- Controleur/main.cpp
+++ Controleur/main.cpp
@@ -2,8 +2,8 @@
 #include <Modele/Evo/Mn.h>
 
 int main() {
-  sim::LightSim lightSim(768, 768, 512, 512, 1, 200);
-  lightSim.run(2e3);
+  sim::LightSim lightSim(768, 768, 512, 512, 10, 200);
+  lightSim.run(1e5);
 
   return EXIT_SUCCESS;
 }
