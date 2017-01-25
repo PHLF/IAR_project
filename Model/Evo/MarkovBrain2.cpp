7bf0f67c7fa435c7d1a704295fde9772495a83fa
Pierre-Henri Le Fur <ph.lefur@gmail.com>
2017-01-25 14:37:01 +0100

    Completed new MB class

--- Model/Evo/MarkovBrain2.cpp
+++ Model/Evo/MarkovBrain2.cpp
@@ -1,20 +1,22 @@
-#include "MarkovBrain2.h"
+﻿#include "MarkovBrain2.h"
+
+using namespace sim;
 
 MarkovBrain2::MarkovBrain2() {}
 
-MarkovBrain2::MarkovBrain2(uint32_t states,
+MarkovBrain2::MarkovBrain2(uint32_t inputs,
                            uint32_t outputs,
-                           bool feedback_loop = false)
-    : _outputs(outputs) {
-  if (feedback_loop) {
-    _states = states + outputs;
-  }
+                           bool feedback_loop)
+    : _outputs(outputs), _feedback_loop(feedback_loop) {
+  _states = feedback_loop ? 1 << (inputs + outputs) : 1 << inputs;
 
   _table = new uint8_t*[_states];
 
   for (uint32_t i = 0; i < _states; ++i) {
     _table[i] = new uint8_t[_outputs];
   }
+
+  random_fill();
 }
 
 MarkovBrain2::MarkovBrain2(const MarkovBrain2& markov_brain) {
@@ -37,12 +39,12 @@ MarkovBrain2& MarkovBrain2::operator=(const MarkovBrain2& markov_brain) {
 
   _table = new uint8_t*[_states];
   for (uint32_t i = 0; i < _states; ++i) {
-    _table = new uint8_t[_outputs];
+    _table[i] = new uint8_t[_outputs];
   }
 
   for (uint32_t i = 0; i < _states; ++i) {
     for (uint32_t j = 0; j < _outputs; ++j) {
-      _table[i][j] = _markov_brain._table[i][j];
+      _table[i][j] = markov_brain._table[i][j];
     }
   }
 
@@ -63,10 +65,12 @@ MarkovBrain2& MarkovBrain2::operator=(MarkovBrain2&& markov_brain) {
   return *this;
 }
 
-ostream& MarkovBrain2::operator<<(ostream& os,
-                                  const MarkovBrain2& markov_brain) {
+std::ostream& ::sim::operator<<(std::ostream& os,
+                                const MarkovBrain2& markov_brain) {
   os << "states: " << markov_brain._states << std::endl;
   os << "outputs: " << markov_brain._outputs << std::endl;
+  os << "feedback_loop: " << std::boolalpha << markov_brain._feedback_loop
+     << std::endl;
 
   for (uint32_t i = 0; i < markov_brain._states; ++i) {
     for (uint32_t j = 0; j < markov_brain._outputs; ++j) {
@@ -80,19 +84,22 @@ ostream& MarkovBrain2::operator<<(ostream& os,
   return os;
 }
 
-istream& MarkovBrain2::operator>>(istream& is, MarkovBrain2& markov_brain) {
+std::istream& ::sim::operator>>(std::istream& is, MarkovBrain2& markov_brain) {
   markov_brain._delete_table();
 
-  is.ignore(1, ' ');
+  is.ignore(' ');
   is >> markov_brain._states;
-  is.ignore(1, '\n');
-  is.ignore(1, ' ');
-  markov_brain._outputs;
-  is.ignore(1, '\n');
+  is.ignore('\n');
+  is.ignore(' ');
+  is >> markov_brain._outputs;
+  is.ignore('\n');
+  is.ignore(' ');
+  is >> std::boolalpha >> markov_brain._feedback_loop;
+  is.ignore('\n');
 
-  _table = new uint8_t*[markov_brain._states];
-  for (uint32_t i = 0; i < _states; ++i) {
-    _table = new uint8_t[markov_brain._outputs];
+  markov_brain._table = new uint8_t*[markov_brain._states];
+  for (uint32_t i = 0; i < markov_brain._states; ++i) {
+    markov_brain._table[i] = new uint8_t[markov_brain._outputs];
   }
 
   for (uint32_t i = 0; i < markov_brain._states; ++i) {
@@ -111,6 +118,49 @@ uint32_t MarkovBrain2::states() const {
 
 uint32_t MarkovBrain2::outputs() const {
   return _outputs;
+}
+
+bool MarkovBrain2::feedback_loop() const {
+  return _feedback_loop;
+}
+
+std::vector<uint8_t> MarkovBrain2::actions(std::vector<uint8_t> state) {
+  std::random_device rd;
+  std::ranlux24 gen(rd());
+  std::uniform_int_distribution<uint8_t> uni_distrib(0, 100);
+
+  uint32_t input_as_int = 0;
+  std::vector<uint8_t> output(_outputs, 0);
+
+  if ((1 << state.size()) == _states) {
+    // vec<bool> {1, 1, ..., 1, 0} to unsigned int
+    input_as_int = accumulate(state.rbegin(), state.rend(), 0,
+                              [](int x, int y) { return (x << 1) + y; });
+
+    for (uint32_t i = 0; i < _outputs; ++i) {
+      if (uni_distrib(gen) <= _table[input_as_int][i]) {
+        output[i] = 1;
+      }
+    }
+  }
+  return output;
+}
+
+uint64_t MarkovBrain2::random_fill() {
+  uint64_t seed;
+  std::random_device rd;
+  std::ranlux48 gen;
+  std::uniform_int_distribution<uint8_t> d_norm{0, 100};
+
+  seed = rd();
+  gen.seed(seed);
+
+  for (uint32_t i = 0; i < _states; ++i) {
+    for (uint32_t j = 0; j < _outputs; ++j) {
+      _table[i][j] = d_norm(gen);
+    }
+  }
+  return seed;
 }
 
 void MarkovBrain2::gaussian_mutation(float alpha) {
--- Model/Evo/MarkovBrain2.h
+++ Model/Evo/MarkovBrain2.h
@@ -1,31 +1,41 @@
-#ifndef MARKOVBRAIN2_H
+﻿#ifndef MARKOVBRAIN2_H
 #define MARKOVBRAIN2_H
 
 #include <Misc/Globals.h>
 
+namespace sim {
 class MarkovBrain2 {
  public:
   MarkovBrain2();
-  MarkovBrain2(uint32_t states, uint32_t outputs);
+  MarkovBrain2(uint32_t states, uint32_t outputs, bool feedback_loop = false);
   MarkovBrain2(const MarkovBrain2& markov_brain);
   MarkovBrain2(MarkovBrain2&& markov_brain);
   ~MarkovBrain2();
 
   uint32_t states() const;
   uint32_t outputs() const;
+  bool feedback_loop() const;
 
-  MarkovBrain2& operator=(MarkovBrain2& markov_brain);
+  MarkovBrain2& operator=(MarkovBrain2&& markov_brain);
   MarkovBrain2& operator=(const MarkovBrain2& markov_brain);
 
-  friend ostream& operator<<(ostream& os, MarkovBrain2 const& markov_brain);
-  friend istream& operator>>(istream& is, MarkovBrain2& markov_brain);
-private:
+  friend std::ostream& operator<<(std::ostream& os,
+                                  MarkovBrain2 const& markov_brain);
+  friend std::istream& operator>>(std::istream& is, MarkovBrain2& markov_brain);
+
+  std::vector<uint8_t> actions(std::vector<uint8_t> state);
+  void gaussian_mutation(float alpha);
+  uint64_t random_fill();
+
+ private:
   uint8_t** _table = nullptr;
 
   uint32_t _states = 0;
   uint32_t _outputs = 0;
 
+  bool _feedback_loop = false;
+
   void _delete_table();
 };
-
+}
 #endif  // MARKOVBRAIN2_H
